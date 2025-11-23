7e63cd203e832ef1dcc75269d5c35c9633aaa4c3
nicolae <nicolae.mariuta@gmail.com>
2025-11-23 19:56:06 +0200

    Commit the initialized project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className="text-red-500">Resto AI</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
